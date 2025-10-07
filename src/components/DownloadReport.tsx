a17e7ffab478cb0ed158f66e78be844337091ff3
Bruno Peres <brunowebdeveloper33@gmail.com>
2025-10-07 10:54:55 +0300

    feat: buttons design improve

--- src/components/DownloadReport.tsx
+++ src/components/DownloadReport.tsx
@@ -180,30 +180,30 @@ const DownloadReport: React.FC = () => {
   return (
     <div className="flex flex-wrap gap-3 mt-6">
       <button
-        className="btn bg-blue-600 text-white hover:bg-blue-700 flex items-center"
+  className="btn flex items-center !text-sm sm:!text-base bg-blue-600 text-white hover:bg-blue-700 py-2 px-3 sm:py-3 sm:px-6"
         onClick={downloadPDF}
       >
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 sm:h-5 sm:w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
           <path fillRule="evenodd" d="M6 2a2 2 0 00-2 2v12a2 2 0 002 2h8a2 2 0 002-2V7.414A2 2 0 0015.414 6L12 2.586A2 2 0 0010.586 2H6zm5 6a1 1 0 10-2 0v3.586l-1.293-1.293a1 1 0 10-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 11.586V8z" clipRule="evenodd" />
         </svg>
         Download PDF
       </button>
       
       <button
-        className="btn bg-green-600 text-white hover:bg-green-700 flex items-center"
+  className="btn flex items-center !text-sm sm:!text-base bg-green-600 text-white hover:bg-green-700 py-2 px-3 sm:py-3 sm:px-6"
         onClick={downloadCSV}
       >
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 sm:h-5 sm:w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
           <path fillRule="evenodd" d="M6 2a2 2 0 00-2 2v12a2 2 0 002 2h8a2 2 0 002-2V7.414A2 2 0 0015.414 6L12 2.586A2 2 0 0010.586 2H6zm5 6a1 1 0 10-2 0v3.586l-1.293-1.293a1 1 0 10-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 11.586V8z" clipRule="evenodd" />
         </svg>
         Download CSV
       </button>
       
       <button
-        className="btn bg-purple-600 text-white hover:bg-purple-700 flex items-center"
+  className="btn flex items-center !text-sm sm:!text-base bg-purple-600 text-white hover:bg-purple-700 py-2 px-3 sm:py-3 sm:px-6"
         onClick={downloadJSON}
       >
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 sm:h-5 sm:w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
           <path fillRule="evenodd" d="M6 2a2 2 0 00-2 2v12a2 2 0 002 2h8a2 2 0 002-2V7.414A2 2 0 0015.414 6L12 2.586A2 2 0 0010.586 2H6zm5 6a1 1 0 10-2 0v3.586l-1.293-1.293a1 1 0 10-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 11.586V8z" clipRule="evenodd" />
         </svg>
         Download JSON
